8f33a8c4ebbf23d754c4568669b93cf5441b931c
student1234-henil <sali.newtech@mail.com>
2020-05-22 15:58:20 +0530

    added chain class

---
+++ Chain.js
@@ -0,0 +1,20 @@
+class Chain{
+
+    constructor(bodyA,bodyB){
+var options = {
+    bodyA:bodyA,
+    bodyB:bodyB,
+    length:10,
+    stiffness:0.04
+}
+this.Chain = Constraint.create(options)
+World.add(world,this.Chain);
+    }
+    display(){
+        var pointA = this.Chain.bodyA.position;
+        var pointB = this.Chain.bodyB.position;
+        strokeWeight(3)
+
+        line(pointA.x,pointA.y,pointB.x,pointB.y)
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -15,6 +16,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
+   
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
@@ -35,6 +37,9 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+
+    constrainedLog = new Log(230,180,80,PI/2);
+    chain = new Chain (bird.body,constrainedLog.body);
 
 }
 
@@ -61,4 +66,7 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    constrainedLog.display();
+    chain.display();
 }
